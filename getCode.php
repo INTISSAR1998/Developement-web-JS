6129a80c9d4249c37f5cf60f2e56e34852082ea7
Intissar EL AHMER <intissarelahmer1998@gmail.com>
2023-04-03 12:24:29 +0100

    Add files via upload

---
+++ getCode.php
@@ -0,0 +1,4 @@
+<?php
+$code=$_GET['code'];
+echo"le code de votre couleur est:".$code;
+?>
---
+++ setColor.php
@@ -0,0 +1,11 @@
+<html>
+	<head>
+		<title> Choisir une couleur</title>
+	</head>
+	<body bgcolor=''>
+		<form action="getCode.php" method="get">
+			<input type="color" name='code'>
+			<input type="submit" name='envoyer'>
+		</form>
+	</body>
+</html>
